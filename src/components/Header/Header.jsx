42d8a29913239e3c2e9d58cf1fd34bc90fe848a4
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-09-14 20:41:18 +0600

    header is added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold text-center mt-9 mb-5'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
